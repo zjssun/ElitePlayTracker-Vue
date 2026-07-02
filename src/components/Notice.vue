aa7c20a660b254ec59ee9be7bbdf185d6d90da69
samrol <1628526495@qq.com>
2026-07-02 17:45:30 +0800

    修改文字

--- src/components/Notice.vue
+++ src/components/Notice.vue
@@ -50,7 +50,7 @@ onBeforeUnmount(() => {
       <p class="notice-title">🛠️域名即将更改⚒️</p>
       <p>可以访问新域名:</p>
       <p class="notice-link"><a class="notice-link" href="https://eptracker.de">https://eptracker.de</a></p>
-      <p>🗑️本域名将在⏱️2026年8月15日下线🔜</p>
+      <p>🗑️本域名将在⏱️2026年7月15日下线🔜</p>
       <p>👁️‍🗨️当旧域名到期后,新域名会加上SSL证书。</p>
       <button @click="closeNotice" class="notice-button">知道了</button>
     </div>
