5bad8c672baf43caabacc329113c2646203db216
samrol <1628526495@qq.com>
2026-07-02 16:43:49 +0800

    新域名通知

--- src/components/Notice.vue
+++ src/components/Notice.vue
@@ -3,8 +3,8 @@ import { computed, ref } from 'vue'
 
 // 2026, 1, 19, 0, 0, 0 表示2026年1月19日0点0分0秒
 // 2026, 1, 31, 23, 59, 59 
-const NOTICE_START_AT = new Date(2026, 1, 19, 0, 0, 0)
-const NOTICE_END_AT = new Date(2026, 1, 31, 23, 59, 59)
+const NOTICE_START_AT = new Date(2026, 7, 2, 0, 0, 0)
+const NOTICE_END_AT = new Date(2026, 7, 14, 23, 59, 59)
 
 const isNoticeInRange = () => {
   const now = Date.now()
@@ -24,8 +24,8 @@ const closeNotice = () => {
     <div class="notice-content">
       <p class="notice-title">🛠️域名即将更改⚒️</p>
       <p>可以访问新域名:</p>
-      <p class="notice-link"><a class="notice-link" href="https://eptracker.xyz">https://eptracker.xyz</a></p>
-      <p>🗑️本域名将在⏱️2025年8月4日下线🔜</p>
+      <p class="notice-link"><a class="notice-link" href="https://eptracker.xyz">https://eptracker.de</a></p>
+      <p>🗑️本域名将在⏱️2025年7月15日下线🔜</p>
       <p>👁️‍🗨️当旧域名到期后,新域名会加上SSL证书。</p>
       <button @click="closeNotice" class="notice-button">知道了</button>
     </div>
